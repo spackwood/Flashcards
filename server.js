1371e574d1dff41329e6af5aa7efbd219eae3f35
Sean Packwood <seanpackwood@Seans-MacBook.local>
2019-04-15 08:42:21 -0700

    update carditem

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 // The following "catch all" route (note the *) is necessary
 // for a SPA's client-side routing to properly work
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 // Configure to use port 3001 instead of 3000 during
--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -5,43 +5,51 @@ import './CardItem.css';
 export default function CardItem(props) {
     return (
         <Card className="card text-white mt-3">
-            <CardBody>
-             <span
-                onClick={props.handleToggle}
-                className="trash float-right far fa-trash-alt"
-            />
-            <CardTitle className="font-weight-bold">Question</CardTitle>
-            <CardText>{props.card.question}</CardText>
-            </CardBody> 
-            <CardBody>
-                <CardTitle
-                    onClick={props.handleToggle}
-                    className="card_title font-weight-bold"
-                >
-                    Show Answer
-                </CardTitle>
-                {props.card.toggle && (
-                    <CardText>
-                        {props.card.answer}
-                        <Button
-                            color="danger"
-                            className="mt-2 mr-4"
-                            id="0"
-                            onClick={props.handleRank}
-                        >
-                            Good
-                        </Button>
-                        <Button
-                            color="danger"
-                            className="mt-2 mr-4"
-                            id="0"
-                            onClick={props.handleRank}
-                            >
-                             Great
-                            </Button>
-                    </CardText>
-                )}
-            </CardBody>
-        </Card>
-    );
+      <CardBody>
+        <span
+          onClick={props.handleToggle}
+          className="trash float-right far fa-trash-alt "
+        />
+        <CardTitle className="font-weight-bold">Question</CardTitle>
+        <CardText>{props.card.question}</CardText>
+      </CardBody>
+      <CardBody>
+        <CardTitle
+          onClick={props.handleToggle}
+          className="card_title font-weight-bold"
+        >
+          Show Answer
+        </CardTitle>
+        {props.card.toggle && (
+          <CardText>
+            {props.card.answer}
+            <Button
+              color="danger"
+              className="mt-2 mr-4"
+              id="0"
+              onClick={props.handleRank}
+            >
+              Bad
+            </Button>
+            <Button
+              color="info"
+              className="mt-2 mr-4"
+              id="1"
+              onClick={props.handleRank}
+            >
+              Good
+            </Button>
+            <Button
+              color="success"
+              className="mt-2 mr-4"
+              id="2"
+              onClick={props.handleRank}
+            >
+              Great
+            </Button>
+          </CardText>
+        )}
+      </CardBody>
+    </Card>
+  );
 }
